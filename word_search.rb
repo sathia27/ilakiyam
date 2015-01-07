b65c6eace29256fa0cc2152c719ff19328d4b787
Sathianarayanan <Sathia>
2015-01-08 00:59:39 +0530

    pure rack app

---
+++ word_search.rb
@@ -0,0 +1,30 @@
+class WordSearch
+  attr_accessor :books, :word, :book_searches
+  def initialize books, word
+    @books = books
+    @word = word
+    @book_searches = []
+  end
+
+  def open_book
+    i = 0
+    @books.each do |book_name|
+      content_array = File.open("books/#{book_name}").readlines
+      #content_array = content_array.collect { |line| line.strip }.select { |line| line != "" }
+      matched_lines = content_array.grep(/#{@word}/)
+      matched_lines.each do |line|
+        index_line = content_array.index(line)
+        first_line_number = index_line - 1
+        if first_line_number > 0
+          @book_searches << [first_line_number+1, content_array[first_line_number]]
+        end
+        @book_searches << [index_line+1, line]
+        if content_array[index_line+2]
+          @book_searches << [index_line+2, content_array[index_line+2]]
+        end
+      end
+      i+=1
+    end
+    @book_searches
+  end
+end
