0c6d0bd1507f988a2b3017357f7ae18df9d4ddd6
Sathianarayanan <Sathia>
2015-01-08 01:07:02 +0530

    pure rack app

--- app.rb
+++ app.rb
@@ -1,13 +1,11 @@
 require 'sinatra'
 require_relative 'word_search'
-class TamilWordSearch < Sinatra::Base
-  get '/' do
-    @file_list = Dir.entries("books").select { |f| !File.directory?(f) }
-    if params[:word] and params[:file] and !params[:word].eql?("") and !params[:file].eql?("")
-      word = WordSearch.new([params[:file]], params[:word])
-      @searches = word.open_book
-      puts @searches.inspect
-    end
-    erb :index
+get '/' do
+  @file_list = Dir.entries("books").select { |f| !File.directory?(f) }
+  if params[:word] and params[:file] and !params[:word].eql?("") and !params[:file].eql?("")
+    word = WordSearch.new([params[:file]], params[:word])
+    @searches = word.open_book
+    puts @searches.inspect
   end
+  erb :index
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 # config.ru
 require './app'
-run TamilWordSearch
+run Sinatra::Application
